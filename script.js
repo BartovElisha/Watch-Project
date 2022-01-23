0a54c8085eb90116b10d1e60e9dbd7f4fd1a95eb
[ElishaBartov] <[elisha.bartov@emerson.com]>
2022-01-24 00:06:59 +0200

    Fixing Secconda snd minutes angel value if value more then 360 deg

--- script.js
+++ script.js
@@ -6,28 +6,22 @@ window.onload = function(){
     let minutes = d.getMinutes();
     let hours = d.getHours();
 
-    seconds = d.getSeconds();
-    sec_deg = seconds*6;  // 1 second = 6 Deg
-
-    minutes = d.getMinutes();
-    min_deg = minutes*6;  // 1 minute = 6 Deg
-
-    hours = d.getHours();
-    hour_deg = hours*30 + minutes*0.5;  // 1 hour = 30 Deg, total 12 hours in 360 degs
-
-    document.querySelector('.seconds-arrow').style.transform = 'rotate('+sec_deg+'deg)';
-    document.querySelector('.minutes-arrow').style.transform = 'rotate('+min_deg+'deg)';
-    document.querySelector('.hours-arrow').style.transform = 'rotate('+hour_deg+'deg)';
-
-
     function fSec(){
         // Get Seconds from Data Object 
         d = new Date();
         seconds = d.getSeconds();
         sec_deg = seconds*6;  // 1 second = 6 Deg
+         if(sec_deg == 360)
+         {
+             sec_deg = 0;
+         }
 
         minutes = d.getMinutes();
         min_deg = minutes*6;  // 1 minute = 6 Deg
+        if(min_deg == 360)
+        {
+            min_deg = 0;
+        }
 
         hours = d.getHours();
         hour_deg = hours*30 + minutes*0.5;  // 1 hour = 30 Deg, total 12 hours in 360 degs
